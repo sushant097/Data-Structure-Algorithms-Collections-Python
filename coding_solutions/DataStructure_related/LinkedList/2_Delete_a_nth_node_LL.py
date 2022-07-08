3a2616124c962e684ff5d70784512c541ddd9f4e
sushant097 <sushant1234gautam@gmail.com>
2022-07-08 22:12:26 +0545

    Update LinkedList reference in readme.md

--- coding_solutions/DataStructure_related/LinkedList/2_Delete_a_nth_node_LL.py
+++ coding_solutions/DataStructure_related/LinkedList/2_Delete_a_nth_node_LL.py
@@ -1,14 +1,42 @@
+'''
+Question:  https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+=> => => =>
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Input: head = [1], n = 1
+Output: []
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+Follow up: Could you do this in one pass?
+
+ 
+'''
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
-    def deleteNode(self, node):
-        """
-        :type node: ListNode
-        :rtype: void Do not return anything, modify node in-place instead.
-        """
-        node.val = node.next.val
-        node.next = node.next.next
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+        
+        while right:
+            left = left.next
+            right = right.next
+        
+        # delete
+        left.next = left.next.next
+        return dummy.next
