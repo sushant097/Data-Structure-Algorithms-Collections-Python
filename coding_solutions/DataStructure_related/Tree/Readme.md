a63d1b6d13e05a4bc5ed169ac5723423e41e5bd2
sushant097 <sushant1234gautam@gmail.com>
2022-07-27 21:56:37 +0545

    Added new Solutions

--- coding_solutions/DataStructure_related/Tree/Readme.md
+++ coding_solutions/DataStructure_related/Tree/Readme.md
@@ -9,3 +9,4 @@
 | 5 | [  Convert Sorted Array to Binary Search Tree](https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/)| [python3](https://github.com/sushant097/Data-Structure-Algorithms-Collections-Python/blob/master/coding_solutions/DataStructure_related/Tree/sortedArrayToBST.py) |
 
 
+
---
+++ coding_solutions/interview_related/MergeSorted.py
@@ -0,0 +1,71 @@
+'''
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+
+'''
+
+# Brute Force Solution
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Time Complexity: O(n); Space Complexity: O(n)
+        i = 0
+        j = 0
+        arr = []
+        while i < m and j < n:
+            if nums1[i] < nums2[j]:
+                arr.append(nums1[i])
+                i += 1
+            else:
+                arr.append(nums2[j])
+                j += 1
+
+        
+        if i >= m and j < n:
+            for a in nums2[j:]:
+                arr.append(a)
+        if j >= n and i < m:
+            for a in nums1[i:]:
+                arr.append(a)
+        
+        for i in range(len(nums1)):
+            nums1[i] = arr[i]
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Time Complexity: O(n); Space: O(1)
+        c = m + n -1
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[c] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[c] = nums2[n-1]
+                n -= 1
+            c -= 1
+        
+        while n > 0:
+            nums1[c] = nums2[n-1]
+            c, n = c-1, n-1
